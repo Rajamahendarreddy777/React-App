f9f4211ffb953471d8b9a97a26aeae2e0c161599
Rajamahendarreddy777 <153619291+Rajamahendarreddy777@users.noreply.github.com>
2024-03-12 00:22:37 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 # Login to Docker Hub using credentials
 docker login -u rajamahendarreddy777 -p dckr_pat_Eg5J3X0S19l0qx3v3AnUI3HoEpU
 
-# Tag the Docker image with a specific repository and tag name
+# Tag the Docker image with a specific repository and tag name(our wish)
 docker tag devops-build_app rajamahendarreddy777/prod
 
 # Push the tagged Docker image to Docker Hub.
